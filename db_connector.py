b45501dc779712ecbc6863bb23a91c234a1e2af6
Aditi <aditi.ch2@gmail.com>
2017-06-01 13:13:00 +0200

    connector
    connectie tussen db en applicatie

---
+++ db_connector.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+def main():
+    db_connect()
+
+def db_connect():
+    database = MySQLdb.connect(host = "localhost",
+                               user = "owe8_1617_grp12",
+                               passwd = "blaat1234",
+                               db = "cytosine.nl")
+    cursor = database.cursor()
+    add_PubMedID = ("INSERT INTO Article"
+                    "(PubMed_ID, Previe_summary)"
+                    "Values(%s, %s)")
+    cursor.execute(add_PubMedID)
+    database.commit()
+    cursor.close()
+
+main()
