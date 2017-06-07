11b4336c44c572888402e32c34cf1db3f906d623
Brent Verpaalen <brentverpaalen95@hotmail.com>
2017-06-07 16:54:47 +0800

    Clean up, Query now returns 100k papers max

--- queryPubMed.py
+++ queryPubMed.py
@@ -1,25 +1,11 @@
 from Bio import Entrez
 
 Entrez.email = "aditi.ch2@gmail.com"     # Always tell NCBI who you are
-#record = Entrez.read(handle)
-handle = Entrez.esearch(db="pubmed", term="Anthocyanins", retmode='xml')
-info = Entrez.esummary(keywds="Stress")
-print(handle)
+handle = Entrez.esearch(db="pubmed", term="Anthocyanins", retmode='xml',retmax=100000)
 records = Entrez.read(handle)
-print(records)
-for record in records:
-    #each record is a Python dictionary or list.
-    print(record)
-    print(records[record])
-#    if "Biopython" in record:
- #       print("Hit")
-  #  else:
-   #     print("No hit")
-#record["IdList"]
-#['1930478','18606172','16403221','16377612','14871561', '14630660', '12230038']
-
-#Acessie in een paper gaan linken
-#
+idsHit = records["IdList"]
+print(idsHit)
+print(len(idsHit))
 
 @staticmethod
 def fetch_articles(id_list):
@@ -28,7 +14,7 @@ def fetch_articles(id_list):
     if id_list:
         for ID in id_list:
             count += 1
-            handle = Entrez.efetch(db="pubmed", id=ID, rettype="medline", retmode="text")
+            handle = Entrez.efetch(db="pubmed", id=ID, rettype="medline", retmode="json",retmax=100000)
             try:
                 article = Medline.parse(handle)
                 articles.append(article)
