44f9da5ff8d2d020b403324ccb118ddce5d43bd9
Brent Verpaalen <brentverpaalen95@hotmail.com>
2017-06-08 12:23:29 +0800

    db connection now inserts a searchword Not tested on server

--- db_connector.py
+++ db_connector.py
@@ -1,19 +1,36 @@
 import mysql.connector as connector
 
-def main():
-    db_connect()
-
 def db_connect():
     database = connector.connect(host = "localhost",
                                user = "owe8_1617_grp12",
                                passwd = "blaat1234",
-                               db = "cytosine.nl")
-    cursor = database.cursor()
-    add_PubMedID = ("INSERT INTO Article"
-                    "(PubMed_ID, Previe_summary)"
-                    "Values(%s, %s)")
-    cursor.execute(add_PubMedID)
-    database.commit()
-    cursor.close()
+                               db = "owe8_167_gr12_2")
+    return database
 
-main()
+def createConnection():
+    connection = database.connect()
+    return connection
+
+def createCursor(connection):
+    cursor = connection.cursor()
+    return cursor
+
+def infoCursor(cursor,connection):
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return result
+
+def submitCursor(cursor,connection):
+    con.commit()
+    cur.close()
+    con.close()
+
+def SubmitSearchWord(searchWord):
+    connection = createConnection()
+    cursor = createCursor(connection)
+    cur.execute("INSERT INTO Search VALUES (NULL,%s)",searchWord)
+    submitCrusor(cursor,connection)
+
+database = db_connect()
+SubmitSearchWord("test")
