7f9710d43bd8420d21d59a98f0697347e8daae9a
Brent Verpaalen <bverpaalen@localhost.localdomain>
2017-06-08 20:03:47 +0800

    Miner can now be used with a file to mine Miner uses first command in command line './queryPubMed /path/to/file'. This file must be in a csv format

--- queryPubMed.py
+++ queryPubMed.py
@@ -1,21 +1,27 @@
+#!/usr/bin/env python3
+
+import sys
 from Bio import Entrez
 from Bio import Medline
 
-def Main():
+def Main(queryFilePath):
     papers = []
     Entrez.email = "aditi.ch2@gmail.com"     # Always tell NCBI who you are
-    handle = Entrez.esearch(db="pubmed", term="Anthocyanins", retmode='xml',retmax=100000)
-    records = Entrez.read(handle)
-    idsHit = records["IdList"]
-    handle.close()
-    for number in range(0,10):
-        hit = idsHit[number]
-        results = fetch_article(hit)
-        for result in results:
-            papers.append(result)
-    for paper in papers:
-        author = paper["AU"]
-        print(author)
+    queryList = FileToList(queryFilePath)
+    for query in queryList:
+        print(query)
+        handle = Entrez.esearch(db="pubmed", term=query, retmode='xml',retmax=100000)
+        records = Entrez.read(handle)
+        idsHit = records["IdList"]
+        handle.close()
+        for number in range(0,10):
+            hit = idsHit[number]
+            results = fetch_article(hit)
+            for result in results:
+                papers.append(result)
+        for paper in papers:
+            author = paper["AU"]
+            print(author)
 
 def fetch_article(idToFetch):
     articles = []
@@ -30,4 +36,12 @@ def fetch_article(idToFetch):
     handle.close()
     return articles
 
-Main()
+def FileToList(filePath):
+    querys = []
+    fileToUse = open(filePath,'r')
+    for line in fileToUse:
+        items = line.replace('\n','').split(',')
+        for item in items:
+            querys.append(item)
+    return querys
+Main(sys.argv[1])
