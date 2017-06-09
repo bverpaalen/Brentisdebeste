462a128117b3dfa31e83ead98082a668d236fce9
Brent Verpaalen <brentverpaalen95@hotmail.com>
2017-06-09 12:24:29 +0800

    Clean up

--- db_connector.py
+++ db_connector.py
@@ -10,15 +10,7 @@ app.config['MYSQL_DATABASE_DB'] = 'owe8_1617_gr12_2'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-def db_connect():
-    database = connector.connect(host = "localhost",
-                               user = "owe8_1617_gr12",
-                               passwd = "blaat1234",
-                               db = "owe8_167_gr12_2")
-    return database
-
 def createConnection():
-    #connection = database.connect()
     connection = mysql.connect()	    
     return connection
 
