8aa855aa232d8f000e683018d6ff58c7915b7d90
Brent Verpaalen <bverpaalen@localhost.localdomain>
2017-06-11 14:13:21 +0800

    Faster and looks better

--- queryPubMed.py
+++ queryPubMed.py
@@ -7,51 +7,74 @@ from Bio import Medline
 def Main(queryFilePath):
     papers = []
     Entrez.email = "aditi.ch2@gmail.com"     # Always tell NCBI who you are
-    queryList = CsvFileToList(queryFilePath)
-    for query in queryList:
-        print(query)
-        handle = Entrez.esearch(db="pubmed", term=query, retmode='xml',retmax=100000)
+    querysAsDicInList = CsvFileToList(queryFilePath)
+    paperResult = RelatedPapers(querysAsDicInList)
+    for query in paperResult:
+        papers = paperResult[query]
+        SubmitPapers(papers,query)
+
+def RelatedPapers(querysAsDicInList):
+    paperResults = {}
+    for query in querysAsDicInList:
+        englishQuery = query["English"] 
+        latinQuery = query["Latin"]
+        dutchQuery = query["Dutch"]
+        handle = Entrez.esearch(db="pubmed", term="("+englishQuery+")OR("+latinQuery+")", retmode='xml',retmax=100000)
         records = Entrez.read(handle)
-        idsHit = records["IdList"]
         handle.close()
+        idsHit = records["IdList"]
+        paperResults[englishQuery] = idsHit
+    return paperResults
+
+def SubmitPapers(papers,query):
+    paperIds = ListToCsv(papers)
+    paper = FetchPaper(paperIds)
+
+def PaperInformation(paper):
+    paperInformation = {}
+    """
         for number in range(0,10):
             hit = idsHit[number]
             results = fetch_article(hit)
             for result in results:
                 papers.append(result)
-        for paper in papers:
-            paperKeys = paper.keys()
-            if 'AU' in paperKeys:
-                author = paper['AU']
-            else:
-                author = "missing"
-            if 'PMID' in paperKeys:
-                link = 'https://www.ncbi.nlm.nih.gov/pubmed/?term='+paper['PMID']
-            else:
-                link = "missing"
-            if 'DP' in paperKeys:
-                datum = paper['DP']
-            else:
-                datum = 'missing'
-            if 'ab' in paperKeys:
-                summary = paper['AB']
-            else:
-                summary = 'missing'
-    print(author)
-    print(link)
-    print(datum)
-    print(summary)
+    """
+    paperKeys = paper.keys()
+    if 'AU' in paperKeys:
+        author = paper['AU']
+    else:
+        author = "missing"
+    if 'PMID' in paperKeys:
+        link = 'https://www.ncbi.nlm.nih.gov/pubmed/?term='+paper['PMID']
+    else:
+        link = "missing"
+    if 'DP' in paperKeys:
+        datum = paper['DP']
+    else:
+        datum = 'missing'
+    if 'ab' in paperKeys:
+        summary = paper['AB']
+    else:
+        summary = 'missing'
+    paperInformation["Author"]
+    paperInformation["Url"]
+    paperInformation["PublicationDate"]
+    paperInformation["Summary"] = summary
+    return paperInformation
 
-def fetch_article(idToFetch):
+def FetchPaper(idToFetch):
     articles = []
+    print(idToFetch)
     handle = Entrez.efetch(db="pubmed", id=idToFetch, rettype="medline", retmode="json",retmax=100000)
     try:
         article = Medline.parse(handle)
     except:
         print("Error can't retrieve: "+idToFetch)
         article = None
+    #TODO find out why for loop is slow, prob pubmed thing
     for results in article:
         articles.append(results)
+    print("Done retrieving")
     handle.close()
     return articles
 
@@ -59,9 +82,18 @@ def CsvFileToList(filePath):
     querys = []
     fileToUse = open(filePath,'r')
     for line in fileToUse:
+        query = {}
         items = line.replace('\n','').split(',')
-        for item in items:
-            querys.append(item)
+        query["Dutch"] = items[0]
+        query["English"] = items[1]
+        query["Latin"] = items[2]
+        querys.append(query)
     return querys
+
+def ListToCsv(items):
+    line = ''
+    for item in items:
+        line += item + ','
+    return line[:-1]
 
 Main(sys.argv[1])
